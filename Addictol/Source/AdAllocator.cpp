1e32b35497e66354e00202e39d251a250c5ad04a
Alexey <vlad.shabalin.1992@mail.ru>
2026-03-28 19:44:53 +0300

    more detailed msgbox out of memory

--- Addictol/Source/AdAllocator.cpp
+++ Addictol/Source/AdAllocator.cpp
@@ -5,6 +5,9 @@
 
 namespace Addictol
 {
+	// Use to convert bytes to KB
+	constexpr static auto AD_DIV = 1024;
+
 	void* ICheckerPointer::CheckPtr(void* lpBlock, size_t nSize) const noexcept
 	{
 		if (!lpBlock)
@@ -14,9 +17,35 @@ namespace Addictol
 			if (!GlobalMemoryStatusEx(&statex))
 				return lpBlock;
 
+			//  https://learn.microsoft.com/en-us/windows/win32/api/sysinfoapi/nf-sysinfoapi-globalmemorystatusex
+			//  Sample output:
+			//  OUT OF MEMORY. A memory allocation failed.
+			//  
+			//  Requested chunk size: 123456 bytes.
+			//  There is       51 percent of memory in use.
+			//  There are 2029968 total KB of physical memory.
+			//  There are  987388 free  KB of physical memory.
+			//  There are 3884620 total KB of paging file.
+			//  There are 2799776 free  KB of paging file.
+			//  There are 2097024 total KB of virtual memory.
+			//  There are 2084876 free  KB of virtual memory.
+			//  There are       0 free  KB of extended memory.
 			AdAssertWithFormattedMessage(lpBlock,
-				"A memory allocation failed.\n\nRequested chunk size: %llu bytes.\n\nAvail memory: %llu bytes, load (%u%%).",
-				nSize, statex.ullAvailPageFile, statex.dwMemoryLoad);
+				"OUT OF MEMORY. A memory allocation failed.\n\n"
+				"Requested chunk size: %llu bytes.\n"
+				"There is  %*ld percent of memory in use.\n"
+				"There are %*I64d total KB of physical memory.\n"
+				"There are %*I64d free  KB of physical memory.\n"
+				"There are %*I64d total KB of paging file.\n"
+				"There are %*I64d free  KB of paging file.\n"
+				"There are %*I64d total KB of virtual memory.\n"
+				"There are %*I64d free  KB of virtual memory.\n"
+				"There are %*I64d free  KB of extended memory.\n",
+				nSize, statex.dwMemoryLoad, 
+				statex.ullTotalPhys / AD_DIV, statex.ullAvailPhys / AD_DIV,
+				statex.ullTotalPageFile / AD_DIV, statex.ullAvailPageFile / AD_DIV,
+				statex.ullTotalVirtual / AD_DIV, statex.ullAvailVirtual / AD_DIV,
+				statex.ullAvailExtendedVirtual / AD_DIV);
 		}
 
 		return lpBlock;
